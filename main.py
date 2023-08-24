bc6766f97df94b4b158d03a710aae8ebe32ee47e
Εμμανουήλ Περπιράκης <139139695+ManosPerpirakes@users.noreply.github.com>
2023-08-24 20:39:27 +0300

    added pause button

--- main.py
+++ main.py
@@ -20,64 +20,81 @@ while closeall != True:
     move_left = False
     move_down = False
     move_right = True
+    pausevar = False
+    pausewait = 0
     while close != True:
+        if pausewait > 10:
+            keys = key.get_pressed()
+            if keys[K_1]:
+                if pausevar:
+                    pausevar = False
+                else:
+                    pausevar = True
+                pausewait = 0
+        pausewait += 1
         w.fill((0, 0, 0))
+        w.blit(font.SysFont('Arial', 30).render('1-pause', True, (255, 0, 0)), (50, 50))
         for i in event.get():
             if i.type == QUIT:
                 close = True
                 closeall = True
             if i.type == KEYDOWN:
-                if (i.key == K_w or i.key == K_UP) and move_down == False:
-                    move_up = True
-                    move_left = False
-                    move_down = False
-                    move_right = False
-                if (i.key == K_a or i.key == K_LEFT) and move_right == False:
-                    move_up = False
-                    move_left = True
-                    move_down = False
-                    move_right = False
-                if (i.key == K_s or i.key == K_DOWN) and move_up == False:
-                    move_up = False
-                    move_left = False
-                    move_down = True
-                    move_right = False
-                if (i.key == K_d or i.key == K_RIGHT) and move_left == False:
-                    move_up = False
-                    move_left = False
-                    move_down = False
-                    move_right = True
-        if move_up:
-            head.y -= 10
-        if move_left:
-            head.x -= 10
-        if move_down:
-            head.y += 10
-        if move_right:
-            head.x += 10
+                if not pausevar:
+                    if (i.key == K_w or i.key == K_UP) and move_down == False:
+                        move_up = True
+                        move_left = False
+                        move_down = False
+                        move_right = False
+                    if (i.key == K_a or i.key == K_LEFT) and move_right == False:
+                        move_up = False
+                        move_left = True
+                        move_down = False
+                        move_right = False
+                    if (i.key == K_s or i.key == K_DOWN) and move_up == False:
+                        move_up = False
+                        move_left = False
+                        move_down = True
+                        move_right = False
+                    if (i.key == K_d or i.key == K_RIGHT) and move_left == False:
+                        move_up = False
+                        move_left = False
+                        move_down = False
+                        move_right = True
+        if not pausevar:
+            if move_up:
+                head.y -= 10
+            if move_left:
+                head.x -= 10
+            if move_down:
+                head.y += 10
+            if move_right:
+                head.x += 10
         draw.rect(w, (0, 0, 255), head)
-        xofpreviousrect = head.x
-        yofpreviousrect = head.y
+        if not pausevar:
+            xofpreviousrect = head.x
+            yofpreviousrect = head.y
+            for i in body:
+                xofthisrect = i.x
+                yofthisrect = i.y
+                i.x = xofpreviousrect
+                i.y = yofpreviousrect
+                xofpreviousrect = xofthisrect
+                yofpreviousrect = yofthisrect
         for i in body:
-            xofthisrect = i.x
-            yofthisrect = i.y
-            i.x = xofpreviousrect
-            i.y = yofpreviousrect
-            xofpreviousrect = xofthisrect
-            yofpreviousrect = yofthisrect
             draw.rect(w, (0, 0, 255), i)
-        counter = 0
-        for i in body:
-            if counter > 0:
-                if i.colliderect(head):
-                    close = True
-            counter += 1
-        if head.colliderect(food):
-            body.append(rect.Rect(xofpreviousrect, yofpreviousrect, 10, 10))
-            food.x = (randint(0, 149) * 10)
-            food.y = (randint(0, 74) * 10)
-        if head.x < 0 or head.x > 1490 or head.y < 0 or head.y > 740:
-            close = True
+        if not pausevar:
+            counter = 0
+            for i in body:
+                if counter > 0:
+                    if i.colliderect(head):
+                        close = True
+                counter += 1
+            if head.colliderect(food):
+                body.append(rect.Rect(xofpreviousrect, yofpreviousrect, 10, 10))
+                food.x = (randint(0, 149) * 10)
+                food.y = (randint(0, 74) * 10)
+            if head.x < 0 or head.x > 1490 or head.y < 0 or head.y > 740:
+                close = True
         draw.rect(w, (0, 255, 0), food)
         display.update()
         clock.tick(20)
